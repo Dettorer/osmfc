06a2cbdaaac42af959902152beb442ab04d71379
Paul Dettorer Hervot <dettorer@dettorer.net>
2023-05-19 15:30:06 +0200

    dependencies: add mapnik for tile generation

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "A tool that generates Anki or PDF flashcards from openstreetmap data";
 
   inputs = {
-    # Use my own nixpkgs fork until the anki and aqt python modules are merged
-    # into nixpkgs (https://github.com/NixOS/nixpkgs/pull/230705)
-    nixpkgs.url = "github:dettorer/nixpkgs/python-add-anki";
+    # Use my own nixpkgs fork until the necessary python modules are available
+    # and non-failing, see the following PRs:
+    # - https://github.com/NixOS/nixpkgs/pull/230705
+    # - https://github.com/NixOS/nixpkgs/pull/231735
+    nixpkgs.url = "github:dettorer/nixpkgs/python-fixes";
     utils.url = "github:numtide/flake-utils";
   };
 
@@ -14,7 +16,12 @@
         pkgs = nixpkgs.legacyPackages.${system};
 
         # Python dependencies for building and using this project
-        python-deps = (ps: with ps; [ genanki osmpythontools pyosmium ]);
+        python-deps = (ps: with ps; [
+          genanki
+          osmpythontools
+          pyosmium
+          python-mapnik
+        ]);
 
         # python-deps + extra dependencies used in the development process
         dev-python-deps = (ps:
